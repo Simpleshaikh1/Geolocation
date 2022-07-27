74e5a8efc1f60b9bf3484b761d567e9ddd723966
Simpleshaikh1 <simpleshaikh2@gmail.com>
2022-07-27 22:29:23 +0100

    Geolocation

---
+++ src/HemisphereDisplay.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ReactDOM } from 'react';
+
+const HemisphereDisplay = () => {
+    return(
+        <div>
+            Hey you are in Northern hemisphere
+        </div>
+    )
+}
+
+export default HemisphereDisplay;
